10bd8da79c7d39de65f079813167ae7bb2f58d86
Hari Mahat <hmahat@gmail.com>
2021-07-29 09:00:08 +0300

    react context and redux

--- chap4/redux-sample/src/store/AppState.ts
+++ chap4/redux-sample/src/store/AppState.ts
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import { PostReducer } from './PostReducer';
+import { UserReducer } from './userReducer';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  user: UserReducer,
+  post: PostReducer,
+});
 
 export type AppState = ReturnType<typeof rootReducer>;
